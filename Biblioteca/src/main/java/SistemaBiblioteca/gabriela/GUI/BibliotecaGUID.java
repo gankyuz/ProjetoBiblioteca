ce9cff3303fddc87bc1d167bf6de945955ec62e2
gabriela.oliveira <gabisoliporto@gmail.com>
2024-09-24 08:51:01 -0300

    att

--- Biblioteca/src/main/java/SistemaBiblioteca/gabriela/GUI/BibliotecaGUID.java
+++ Biblioteca/src/main/java/SistemaBiblioteca/gabriela/GUI/BibliotecaGUID.java
@@ -7,24 +7,27 @@ import SistemaBiblioteca.gabriela.Sistema.SistemaBiblioteca;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class BibliotecaGUID extends JFrame {
     JLabel linha1, linha2;
-    ImageIcon bibliotecaPrincipal = new ImageIcon("./imgs/bibvirtual.jpg");
-    ImageIcon usuario = new ImageIcon("./imgs/cadastroUsup.png");
-    ImageIcon livro = new ImageIcon("./imgs/cadastroLivro.png");
-    ImageIcon remove = new ImageIcon("./imgs/removeLivro.png");
-    ImageIcon pesqImg = new ImageIcon("./imgs/lupa.png");
+    ImageIcon bibliotecaPrincipal = new ImageIcon(new ImageIcon("./imgs/livroprincipal.png").getImage().getScaledInstance(150,110, Image.SCALE_SMOOTH));
+    ImageIcon usuario = new ImageIcon(new ImageIcon("./imgs/do-utilizador.png").getImage().getScaledInstance(50,50, Image.SCALE_SMOOTH));
+    ImageIcon livroempres = new ImageIcon(new ImageIcon("./imgs/livroempres.png").getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH));
+    ImageIcon livro = new ImageIcon(new ImageIcon("./imgs/book.png").getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH));
+    ImageIcon remove = new ImageIcon(new ImageIcon("./imgs/lixo.png").getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH));
+    ImageIcon pesqImg = new ImageIcon(new ImageIcon("./imgs/pesquisar-alt.png").getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH));
     JButton botaoCadastrarUsu, botaoCadastrarLi, botaoEmprestimo, botaoPesquisar, botaoRemover;
     SistemaBiblioteca sistema = new SistemaBiblioteca();
     public BibliotecaGUID(){
         setTitle("Minha Biblioteca");
+        //System.out.println("++++>"+new File("./imgs/cadastroUsup.png").getAbsolutePath());
         setSize(800,600); //tamanho da janela
-        setLocation(150, 150);
-        setResizable(false);
-        getContentPane().setBackground(Color.black);
+        setLocationRelativeTo(null);
+        setResizable(true);
+        getContentPane().setBackground(Color.white);
         linha1 = new JLabel("Biblioteca Elevando a Leitura", JLabel.CENTER);
-        linha1.setForeground(Color.white);
+        linha1.setForeground(Color.black);
         linha1.setFont(new Font("Serif", Font.BOLD, 24));
         linha2 = new JLabel(bibliotecaPrincipal, JLabel.CENTER);
 
@@ -34,7 +37,7 @@ public class BibliotecaGUID extends JFrame {
         botaoCadastrarLi = new JButton("Cadastrar Livro",livro);
         botaoCadastrarLi.addActionListener(new BibliotecaAddLivroController(sistema,this));
 
-        botaoEmprestimo = new JButton("Empréstimo", null);
+        botaoEmprestimo = new JButton("Empréstimo", livroempres);
         botaoEmprestimo.addActionListener(new BibliotecaEmprestimoController(sistema, this));
 
 
